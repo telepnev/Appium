c299a830fe1b87b6fb8fac179771dd55c7d84f81
Evgeniy Telepnev <telepnef@yandex.ru>
2021-07-16 19:45:30 +0300

    Refactoring finish ...

---
+++ src/tests/ArticleTests.java
@@ -0,0 +1,40 @@
+package tests;
+
+import lib.CoreTestCase;
+import lib.ui.ArticlePageObject;
+import lib.ui.SearchPageObject;
+import org.junit.Test;
+
+public class ArticleTests extends CoreTestCase {
+    @Test
+    public void testCompareArticleTitle() {
+        SearchPageObject SearchPageObject = new SearchPageObject(driver);
+
+        SearchPageObject.initSearchInput();
+        SearchPageObject.typeSearchLine("Java");
+        SearchPageObject.clickByArticleWithSubstring("Java (programming language)");
+
+        ArticlePageObject ArticlePageObject = new ArticlePageObject(driver);
+        ArticlePageObject.waitForTitleElement();
+        String article_title = ArticlePageObject.getArticleTitle();
+
+        assertEquals(
+                "The title different",
+                "Java (programming language)",
+                article_title
+        );
+    }
+    @Test
+    public void testSwipeArticleTitle() {
+        SearchPageObject SearchPageObject = new SearchPageObject(driver);
+
+        SearchPageObject.initSearchInput();
+        SearchPageObject.typeSearchLine("Appium");
+        SearchPageObject.clickByArticleWithSubstring("Appium");
+
+        ArticlePageObject ArticlePageObject = new ArticlePageObject(driver);
+        ArticlePageObject.waitForTitleElement();
+        ArticlePageObject.swipeToFooter();
+    }
+
+}
---
+++ src/tests/SearchTests.java
@@ -0,0 +1,53 @@
+package tests;
+
+import lib.CoreTestCase;
+import lib.ui.SearchPageObject;
+import org.junit.Test;
+
+public class SearchTests extends CoreTestCase {
+    @Test
+    public void testSearch() {
+
+        SearchPageObject SearchPageObject = new SearchPageObject(driver);
+
+        SearchPageObject.initSearchInput();
+        SearchPageObject.typeSearchLine("Java");
+        SearchPageObject.waitForSearchResult("Java (programming language)");
+    }
+
+    @Test
+    public void testCancelSearch() {
+        SearchPageObject SearchPageObject = new SearchPageObject(driver);
+
+        SearchPageObject.initSearchInput();
+        SearchPageObject.waitForCancelButtonToAppear();
+        SearchPageObject.clickCancelSearch();
+        SearchPageObject.waitForCancelToDisAppear();
+    }
+
+    @Test
+    public void testAmountOfNotEmptySearch() {
+        SearchPageObject SearchPageObject = new SearchPageObject(driver);
+        SearchPageObject.initSearchInput();
+        String article = "The Offspring";
+        SearchPageObject.typeSearchLine(article);
+        int amount_of_search_results = SearchPageObject.getAmountOfFoundArticles();
+
+        assertTrue(
+                "We found too few results!",
+                amount_of_search_results > 0
+        );
+    }
+
+    @Test
+    public void testAmountOfEmptySearch() {
+        SearchPageObject SearchPageObject = new SearchPageObject(driver);
+        SearchPageObject.initSearchInput();
+        String article = "kjlhhvlaiwhupa666";
+        SearchPageObject.typeSearchLine(article);
+        SearchPageObject.waitForEmptyResultsLabel();
+        SearchPageObject.assertThereIsNoResultOfSearch();
+    }
+
+
+}
